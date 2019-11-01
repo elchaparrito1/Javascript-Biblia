ea2de31894f26f1d8c5f91279e71e3912186318d
Mitchell Waite <mitchellwaite@Aubreys-MacBook-Pro.local>
2019-11-01 10:06:52 -0600

    added chapter 5 notes

---
+++ Chapter 5.js
@@ -0,0 +1,97 @@
+//Chapter 5 - Statements
+    //Remember, expressions are evaluated to produce a value.
+    //But, statements are executed to make something happen.
+
+    //JS programs are nothing more than a sequence of statements to execute.
+    //And it executes these statements in the order they're written.
+
+//5.1 Expression Statements
+    //The simplest kinds of statements in JS are expressions that have side effects.
+        //For example, an assignment statement like many covered in chapt 4:
+        greeting = "Hello " + name;
+        
+        /*The -- and ++ operators are related to assignment statements. These have the side
+        effect of changing a variable value:*/
+            counter++;
+        //Delete has the side effect of deleting an object property.
+        //Thus, it is almost always used as a statement:
+        delete o.x;
+
+        //Or function calls are another example of expression statements:
+            alert("Hello");
+            window.close();
+        /*These client-side function calls are expressions, but they have side effects
+        that affect the web browser and are used as statements*/
+
+//5.2 Compound and Empty Statements
+    //A comma operator combines multiple expressions into a single line:
+    i=1, j=2, k=3;
+
+    /*A statement block combines multiple statements into a single compound statement,
+    which are simply enclosed in curly braces:*/
+    {
+        x = Math.PI;
+        cs = Math.cos(x);
+        console.log("What does cos do on the Math object?");
+    }
+
+    //A compound statement allows you to use multiple statements where JS syntax expects a singl statement.
+
+    //The empty statement is the opposite. 
+    //It allows you to include no statements where one is expected. It looks like this:
+    ;
+
+//5.3 Declaration Statements
+    //var, let, const, and function are declaration statements.
+    //These statements define identifiers that can be used elsewhere.
+    /*They don't do much on their own, but they are important in defining 
+    the meaning of other statements in your program.*/
+
+    //If a variable appears in a function scope it defines local variables.
+    //If it appears in the global scope, it becomes a part of the global object.
+    //Unlike other global properties, however, properties created with var cannot be deleted.
+
+    /*Note that if no initializer is specified for a variable statement, the variable's initial
+    value is undefined. As described in 3.10.1, variables are defined throughout the script or
+    function in which they're declared. They are hoisted up to the start of the script or function.
+    Initialization, however, occurs at the location of the variable statement, and the value
+    of the variable is undefined before that point in the code:*/
+
+    //recall:
+    runScope = () => {
+        console.log(variable);
+        let variable = "local";
+      };
+      
+      runScope(); //returns ReferenceError: Cannot access 'variable' before initialization
+
+    //This would still return the same error:
+    let variable = "global";
+
+    runScope = () => {
+        console.log(variable);
+        let variable = "local";
+      };
+      
+      runScope(); 
+    /*b/c again, 'variable' is being declared in the local function and then hoisted, so 
+    runScope knows not to go outside of its scope for that variable, but again, initialization
+    happens at the location of the var statement, and the value of the variable is undefined before
+    that point in the code*/
+    
+    //Options for avoiding reference errors:
+    runScope = () => {
+        let variable = "local";
+        console.log(variable);
+      };
+      
+      runScope(); 
+
+    //or
+    let variable = "global";
+
+    runScope = () => {
+        console.log(variable);
+      };
+      
+      runScope(); 
